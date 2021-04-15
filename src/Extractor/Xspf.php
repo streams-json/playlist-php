4a8df0af6e3d59b425b9a94a08280fcb9332bbc4
François LASSERRE <gpg@francoislasserre.fr>
2021-04-15 09:39:35 +0200

    Remove useless variables

--- src/Extractor/Xspf.php
+++ src/Extractor/Xspf.php
@@ -11,19 +11,19 @@ class Xspf extends AbstractExtractor
     protected function extractStream(\stdClass $station, \stdClass $stream): void
     {
         $xml = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><playlist xmlns="http://xspf.org/ns/0/" version="1"></playlist>');
-        $title = $xml->addChild('title', $station->name);
-        $creator = $xml->addChild('creator');
+        $xml->addChild('title', $station->name);
+        $xml->addChild('creator');
         $tracklist = $xml->addChild('trackList');
         $track = $tracklist->addChild('track');
 
         $urls = is_array($stream->url) ? $stream->url : [$stream->url];
 
         foreach ($urls as $url) {
-            $location = $track->addChild('location', $url);
+            $track->addChild('location', $url);
         }
 
-        $annotation = $track->addChild('annotation', "Stream Title: ".$station->name.\PHP_EOL."Stream Description: ".$station->description.\PHP_EOL."Stream Genre: ".$station->genre);
-        $info = $track->addChild('info', $station->url);
+        $track->addChild('annotation', "Stream Title: ".$station->name.\PHP_EOL."Stream Description: ".$station->description.\PHP_EOL."Stream Genre: ".$station->genre);
+        $track->addChild('info', $station->url);
 
         $this->writeFile($xml->asXML(), $station->name, $stream->bitrate, $stream->type);
     }
