b4aa888fdfefde821f5f2493a63f51dcce0fbbbf
François LASSERRE <198672+ChoiZ@users.noreply.github.com>
2021-04-15 09:40:05 +0200

    Some fixes (#2)
    * Ignore files generated and .vscode
    
    * Fix typo
    
    * Update \n by \PHP_EOL

--- src/Extractor/Xspf.php
+++ src/Extractor/Xspf.php
@@ -10,24 +10,24 @@ class Xspf extends AbstractExtractor
 
     protected function extractStream(\stdClass $station, \stdClass $stream): void
     {
-        $content = '<?xml version="1.0" encoding="UTF-8"?>'."\n";
-        $content.= '<playlist xmlns="http://xspf.org/ns/0/" version="1">'."\n";
-        $content.= '  <title>'.$station->name.'</title>'."\n";
-        $content.= '  <creator/>'."\n";
-        $content.= '  <trackList>'."\n";
-        $content.= '    <track>'."\n";
+        $content = '<?xml version="1.0" encoding="UTF-8"?>'.\PHP_EOL;
+        $content.= '<playlist xmlns="http://xspf.org/ns/0/" version="1">'.\PHP_EOL;
+        $content.= '  <title>'.$station->name.'</title>'.\PHP_EOL;
+        $content.= '  <creator/>'.\PHP_EOL;
+        $content.= '  <trackList>'.\PHP_EOL;
+        $content.= '    <track>'.\PHP_EOL;
 
         $urls = is_array($stream->url) ? $stream->url : [$stream->url];
 
         foreach ($urls as $url) {
-            $content .= "      <location>".$url."</location>\n";
+            $content .= "      <location>".$url."</location>".\PHP_EOL;
         }
 
-        $content.= "      <annotation>Stream Title: ".$station->name."\nStream Description: ".$station->description."\nStream Genre: ".$station->genre."</annotation>\n";
-        $content.= "      <info>".$station->url."</info>\n";
-        $content.= '    </track>'."\n";
-        $content.= '  </trackList>'."\n";
-        $content.= '</playlist>'."\n";
+        $content.= "      <annotation>Stream Title: ".$station->name.\PHP_EOL."Stream Description: ".$station->description.\PHP_EOL."Stream Genre: ".$station->genre."</annotation>".\PHP_EOL;
+        $content.= "      <info>".$station->url."</info>".\PHP_EOL;
+        $content.= '    </track>'.\PHP_EOL;
+        $content.= '  </trackList>'.\PHP_EOL;
+        $content.= '</playlist>'.\PHP_EOL;
 
         $this->writeFile($content, $station->name, $stream->bitrate, $stream->type);
     }
